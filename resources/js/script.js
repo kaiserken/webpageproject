b58c9290f73481ff9be67de57bdcd0bd1bf8b1da
Ken Kaiser <kkaiser@doublehelixllc.com>
2016-05-30 16:22:22 -0700

    add smooth scrolling

--- resources/js/script.js
+++ resources/js/script.js
@@ -19,5 +19,20 @@ $(document).ready(function(){
     $('html, body').animate({scrollTop: $(".js--section--features").offset().top}, 1000);
   });
 
+  //navigation scroll
+  $(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
 
 });
