31b5a8a1b6be37bee8578e3c681a3541da7275d6
Ken Kaiser <kkaiser@doublehelixllc.com>
2016-05-30 16:08:49 -0700

    setting up scrolling

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,4 +1,23 @@
 $(document).ready(function(){
+// for sticky navigation
+  $('.js--section--features').waypoint(function(direction){
+    if (direction == "down"){
+      $('nav').addClass('sticky');
+    } else {
+      $('nav').removeClass('sticky');
+    }
+  }, {
+    offset:"60px;"
+  });
+// scroll on buttons
+
+  $('.js--scroll-to-plans').click(function(){
+    $('html, body').animate({scrollTop: $(".js--section--plans").offset().top}, 1000);
+  });
+
+  $('.js--scroll-to-start').click(function(){
+    $('html, body').animate({scrollTop: $(".js--section--features").offset().top}, 1000);
+  });
 
 
 });
